f4934a1727aa7e1010b752bb22c7d836a21d1a2b
Usman <nazirusman131@gmail.com>
2022-01-03 22:26:38 +0500

    updated

--- src/containers/Calls.tsx
+++ src/containers/Calls.tsx
@@ -8,22 +8,22 @@ import BeatLoader from "react-spinners/BeatLoader";
 import swal from 'sweetalert';
 
 
-// type Note = {
-//   id:  String
-//   content: String
-// };
-// type Call =  {
-//   id: String 
-//   direction: String // "inbound" or "outbound" call
-//   from: String // Caller's number
-//   to: String // Callee's number
-//   duration: number // Duration of a call (in seconds)
-//   is_archived: Boolean // Boolean that indicates if the call is archived or not
-//   call_type: String // The type of the call, it can be a missed, answered or voicemail.
-//   via: String // Aircall number used for the call.
-//   created_at: String // When the call has been made.
-//   notes: Note[] // Notes related to a given call
-// }
+type Note = {
+  id:  String
+  content: String
+};
+type Call =  {
+  id: String 
+  direction: String // "inbound" or "outbound" call
+  from: String // Caller's number
+  to: String // Callee's number
+  duration: number // Duration of a call (in seconds)
+  is_archived: Boolean // Boolean that indicates if the call is archived or not
+  call_type: String // The type of the call, it can be a missed, answered or voicemail.
+  via: String // Aircall number used for the call.
+  created_at: String // When the call has been made.
+  notes: Note[] // Notes related to a given call
+}
 
 export default function Calls() {
 
