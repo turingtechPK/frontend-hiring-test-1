d88baf40b6d048e08d753bb0f1b706e5e1c95b77
Ahsan Khan <imkhanahsan@yahoo.com>
2023-05-27 13:37:40 +0500

    app setup

--- test/tailwind.config.js
+++ test/tailwind.config.js
@@ -1,16 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        primary: '#4F46F8',
+        login: '#448ef7',
+        bg1: '#f3eeee'
+      },
+      fontFamily: {
+        avenirblack: ['AvenirLTStd-Black', 'Helvetica', 'Arial', 'sans-serif'],
+        avenirbook: ['AvenirLTStd-Book', 'Helvetica', 'Arial', 'sans-serif'],
+        avenirroman: ['AvenirLTStd-Roman', 'Helvetica', 'Arial', 'sans-serif'],
       },
     },
   },
