50c1ccdd8befcb8592c16d10dce46745c6589300
m-muaz123 <muazmughal22@gmail.com>
2023-06-18 18:19:51 +0500

    task/basic login form created

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,14 +1,39 @@
-import React from 'react'
+import React from 'react';
+import { useForm } from 'react-hook-form';
+import { TextField, Button, Box } from '@mui/material';
+import { LoginStyled } from './login.style';
 
-import { LoginStyled } from './login.style'
+export const Login = () =>{
+  const { register, handleSubmit, formState: { errors } } = useForm();
 
-export const Login = () => {
+  const onSubmit = (data:any) => {
+    console.log(data); // You can replace this with your desired logic for handling form submission
+  };
+
   return (
     <LoginStyled>
-      <div>
-        <h1>Login Form</h1>
-      </div>
+    <form onSubmit={handleSubmit(onSubmit)}>
+        <Box className="input-fields">
+                <TextField
+                id="email"
+                label="Email"
+                variant="outlined"
+                fullWidth
+                {...register('email', { required: 'Email is required', pattern: /^\S+@\S+$/i })}
+                error={errors.email ? true : false}
+                />
+                <TextField
+                id="password"
+                label="Password"
+                type="password"
+                variant="outlined"
+                fullWidth
+                {...register('password', { required: 'Password is required' })}
+                error={errors.password ? true : false}
+                />
+        </Box>
+      <Button type="submit" variant="contained" color="primary">Login</Button>
+    </form>
     </LoginStyled>
-  )
+  );
 }
-
