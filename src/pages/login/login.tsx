e26877d47bb51806f2bb94754265befc1b517946
m-muaz123 <muazmughal22@gmail.com>
2023-06-18 17:46:44 +0500

    task/login page setup

---
+++ src/pages/login/login.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import { LoginStyled } from './login.style'
+
+export const Login = () => {
+  return (
+    <LoginStyled>
+      <div>
+        <h1>Login Form</h1>
+      </div>
+    </LoginStyled>
+  )
+}
+
