b9f694ccf773b49bcf831145e7863c9c7c6698c6
Ali Nauman Qureshi <alinauman97@outlook.com>
2023-10-14 14:04:29 +0500

    Setup layout

--- turing-calls/src/App.tsx
+++ turing-calls/src/App.tsx
@@ -1,8 +1,40 @@
+import { Layout, Space } from 'antd'
+
 import './App.css'
 import { AppRouter } from './feature/router/AppRouter.tsx'
+import { AppHeader } from './components/AppHeader.tsx'
+
+const { Header, Content } = Layout
+
+const headerStyle: React.CSSProperties = {
+  textAlign: 'center',
+  color: '#fff',
+  height: 64,
+  paddingInline: 50,
+  lineHeight: '64px',
+  backgroundColor: 'white',
+}
+
+const contentStyle: React.CSSProperties = {
+  textAlign: 'center',
+  minHeight: 120,
+  lineHeight: '120px',
+  color: '#fff',
+}
 
 function App() {
-  return <AppRouter />
+  return (
+    <Space direction="vertical" style={{ width: '100%' }} size={[0, 48]}>
+      <Layout>
+        <Header style={headerStyle}>
+          <AppHeader />
+        </Header>
+        <Content style={contentStyle}>
+          <AppRouter />
+        </Content>
+      </Layout>
+    </Space>
+  )
 }
 
 export default App
