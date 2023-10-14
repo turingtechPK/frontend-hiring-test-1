223da1606c6b3dc1d969ceba9249e6ef303985c3
Ali Nauman Qureshi <alinauman97@outlook.com>
2023-10-14 15:46:41 +0500

    Add types for login functionality

---
+++ turing-calls/src/feature/login/types.ts
@@ -0,0 +1,27 @@
+export type LoginInput = {
+  username: string
+  password: string
+}
+
+export type Login = {
+  access_token: string
+  refresh_token: string
+  user: User
+}
+
+export type LoginResponse = {
+  login: Login
+}
+
+export type User = {
+  id: string
+  username: string
+}
+
+export type LoginContextType = {
+  user: User | null
+  accessToken: string | null
+  refreshToken: string | null
+  signIn: (credentials: LoginInput, callback: VoidFunction) => void
+  signOut: (callback: VoidFunction) => void
+}
