5c94d52d6ea3089023164068a881e226c1a1f27e
Ali Nauman Qureshi <alinauman97@outlook.com>
2023-10-14 15:47:54 +0500

    Use BrowserRouter directly instead of calling createBrowserRouter

--- turing-calls/src/App.tsx
+++ turing-calls/src/App.tsx
@@ -1,7 +1,9 @@
 import { Layout, Space } from 'antd'
 
 import './App.css'
-import { AppRouter } from './feature/router/AppRouter.tsx'
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
+import { Login } from './feature/login/Login.tsx'
+import { Calls } from './feature/calls/Calls.tsx'
 import { AppHeader } from './components/AppHeader.tsx'
 
 const { Header, Content } = Layout
@@ -24,16 +26,22 @@ const contentStyle: React.CSSProperties = {
 
 function App() {
   return (
-    <Space direction="vertical" style={{ width: '100%' }} size={[0, 48]}>
-      <Layout>
-        <Header style={headerStyle}>
-          <AppHeader />
-        </Header>
-        <Content style={contentStyle}>
-          <AppRouter />
-        </Content>
-      </Layout>
-    </Space>
+    <BrowserRouter>
+      <Space direction="vertical" style={{ width: '100%' }} size={[0, 48]}>
+        <Layout style={{ height: '100vh' }}>
+          <Header style={headerStyle}>
+            <AppHeader />
+          </Header>
+          <Content style={contentStyle}>
+            <Routes>
+              <Route path="/login" element={<Login />} />
+              <Route path="/calls" element={<Calls />} />
+              <Route path="/" element={<Navigate to="/calls" />} />
+            </Routes>
+          </Content>
+        </Layout>
+      </Space>
+    </BrowserRouter>
   )
 }
 
