b48f861b915f5791428d6d511cb91c306dd693c9
Ali Nauman Qureshi <alinauman97@outlook.com>
2023-10-14 15:50:35 +0500

    Conditionally render Logout button, navigate to Login page after clicking

--- turing-calls/src/components/AppHeader.tsx
+++ turing-calls/src/components/AppHeader.tsx
@@ -1,19 +1,33 @@
 import { Button, Flex } from 'antd'
 
 import turingLogo from '../assets/TT Logo.png'
+import { useLogin } from '../feature/login/useLogin.ts'
+import { useNavigate } from 'react-router-dom'
 
 export const AppHeader: React.FC = () => {
+  const context = useLogin()
+  const navigate = useNavigate()
+
+  const handleLogout = () => {
+    context.signOut(() => {
+      navigate('/login')
+    })
+  }
+
   return (
     <Flex justify="space-between" align="center">
       <img src={turingLogo} alt="Turing Technologies" width={400} />
-      <Button
-        style={{
-          backgroundColor: 'var(--color-primary',
-          color: 'white',
-        }}
-      >
-        Logout
-      </Button>
+      {context.user && (
+        <Button
+          style={{
+            backgroundColor: 'var(--color-primary',
+            color: 'white',
+          }}
+          onClick={handleLogout}
+        >
+          Logout
+        </Button>
+      )}
     </Flex>
   )
 }
