4c5d2a3da10459b14846bf5dcfa6b220ba642d52
Ali Nauman Qureshi <alinauman97@outlook.com>
2023-10-14 17:13:18 +0500

    Render Calls list

--- turing-calls/src/feature/calls/Calls.tsx
+++ turing-calls/src/feature/calls/Calls.tsx
@@ -1,3 +1,99 @@
+import { Alert, Button, Table } from 'antd'
+import { ColumnsType } from 'antd/es/table'
+import { useCalls } from './useCalls.ts'
+import { Call } from './types.ts'
+
+const columns: ColumnsType<Call> = [
+  {
+    title: 'Call Type',
+    dataIndex: 'call_type',
+    key: 'call_type',
+  },
+  {
+    title: 'Direction',
+    dataIndex: 'direction',
+    key: 'direction',
+  },
+  {
+    title: 'Duration',
+    dataIndex: 'duration',
+    key: 'duration',
+    render: (duration: Call['duration']) => {
+      const minutes = Number.parseInt(duration / 60 + '')
+      const remainderSeconds = duration % 60
+
+      return (
+        <>
+          <span>
+            {minutes} minutes {remainderSeconds} seconds
+          </span>
+          <br />
+          <span style={{ color: '#4166e9' }}>({duration} seconds)</span>
+        </>
+      )
+    },
+  },
+  {
+    title: 'From',
+    key: 'from',
+    dataIndex: 'from',
+  },
+  {
+    title: 'To',
+    key: 'to',
+    dataIndex: 'to',
+  },
+  {
+    title: 'Via',
+    key: 'via',
+    dataIndex: 'via',
+  },
+  {
+    title: 'Created At',
+    key: 'created_at',
+    dataIndex: 'created_at',
+    render: (createdAt: Call['created_at']) => (
+      <span>{new Date(createdAt).toLocaleDateString()} </span>
+    ),
+  },
+  {
+    title: 'Status',
+    key: 'is_archived',
+    dataIndex: 'is_archived',
+    render: (isArchived: Call['is_archived']) => (
+      <span>{isArchived ? 'Archived' : 'Unarchived'}</span>
+    ),
+  },
+  {
+    title: 'Actions',
+    render: () => (
+      <Button
+        style={{ color: 'white', backgroundColor: `var(--color-primary)` }}
+      >
+        Add Note
+      </Button>
+    ),
+  },
+]
+
 export const Calls: React.FC = () => {
-  return <h1>Calls</h1>
+  const { data, error, isLoading } = useCalls(0, 10)
+  const calls = data?.paginatedCalls.nodes
+
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h1>Turing Technologies Frontend Test</h1>
+      {isLoading ? (
+        'Loading...'
+      ) : error ? (
+        <Alert
+          message="Something went wrong!"
+          description="Something went wrong while fetching Calls :("
+          type="error"
+        />
+      ) : (
+        <Table columns={columns} dataSource={calls} rowKey={r => r.id} />
+      )}
+    </div>
+  )
 }
