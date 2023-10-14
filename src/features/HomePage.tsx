20f89c7da94e38145458d30e495f3c1516193dff
Wajahat Zia <wzia.bscs16seecs@seecs.edu.pk>
2023-10-14 17:35:43 +0500

    feat: added antd and styled components

---
+++ src/features/HomePage.tsx
@@ -0,0 +1,14 @@
+import { Button, ConfigProvider } from "antd"
+
+import theme from '@/theme/themeConfig';
+
+
+export const HomePage: React.FC = () => {
+  return (
+    <ConfigProvider theme={theme}>
+    <div className="App">
+      <Button type="primary">Button</Button>
+    </div>
+  </ConfigProvider>
+  )
+}
