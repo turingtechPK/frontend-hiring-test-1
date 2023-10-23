4a6135c664d118c8941c05396409a259b166c04d
OBAID-PLT <obaid.rehman@prettylittlething.com>
2023-10-23 13:27:21 +0500

    Added folder structure

---
+++ calling-directories/components/CallList.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export const CallList = () => {
+  return <div>CallList</div>
+}
---
+++ calling-directories/jest.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '\\.(scss|css|jpg|png)$': 'identity-obj-proxy',
+  },
+}
